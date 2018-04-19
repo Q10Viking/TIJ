5279f61b68c5b1d627e24844a365e87a19c9cb87
Q10Viking <1403090523@cau.edu.cn>
2018-04-19 20:11:12 +0800

    update code files

--- datetime/E09/MainTest.java
+++ datetime/E09/MainTest.java
@@ -8,6 +8,18 @@ public class MainTest{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd",Locale.US);
         LocalDate date = LocalDate.parse("2005-Jun-12",formatter);
         System.out.println(date);
+
+        formatter = DateTimeFormatter.ofPattern("yyyy-M-dd",Locale.US);
+        date = LocalDate.parse("2018-4-19",formatter);
+        System.out.println(date);
+
+        formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd",Locale.US);
+        date = LocalDate.parse("2018-04-20",formatter);
+        System.out.println(date);
+
+        formatter = DateTimeFormatter.ofPattern("yyyy-MMMM-dd",Locale.US);
+        date = LocalDate.parse("2018-September-21",formatter);
+        System.out.println(date);
     }
 
 }
