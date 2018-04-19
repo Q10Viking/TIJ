b5a88802882d4b68d18f1ea475b6a3ab0dae22cd
Q10Viking <1403090523@cau.edu.cn>
2018-04-19 21:02:50 +0800

    update code

--- datetime/E11/TemporalAdjustersEx.java
+++ datetime/E11/TemporalAdjustersEx.java
@@ -8,18 +8,19 @@ public class TemporalAdjustersEx{
     public static void main(String[] args){
         LocalDate date = LocalDate.now();
         DayOfWeek dotw = date.getDayOfWeek();
-        System.out.printf("%s is on a %s%n",date,dotw);
 
-        System.out.printf("first day of Month: %s%n",date.with(TemporalAdjusters.firstDayOfMonth()));
+        System.out.printf("%s is on a %s%n",date,dotw);   //2018-04-19 is on a THURSDAY
+
+        System.out.printf("first day of Month: %s%n",date.with(TemporalAdjusters.firstDayOfMonth()));  //first day of Month: 2018-04-01
 
         System.out.printf("first Monday of Month: %s%n",
-                date.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY)));
+                date.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY)));   //first Monday of Month: 2018-04-02
 
         System.out.printf("last day of Month: %s%n",
-                date.with(TemporalAdjusters.lastDayOfMonth()));
+                date.with(TemporalAdjusters.lastDayOfMonth())); //last day of Month: 2018-04-30
 
         System.out.printf("first day of next Month: %s%n",
-                date.with(TemporalAdjusters.firstDayOfNextMonth()));
+                date.with(TemporalAdjusters.firstDayOfNextMonth())); //first day of next Month: 2018-05-01
 
 
     }
