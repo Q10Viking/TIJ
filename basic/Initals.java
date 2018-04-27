d6de65198aea5f8107bbb303749dbc45b695b6be
Q10Viking <1403090523@cau.edu.cn>
2018-04-27 18:42:40 +0800

    hello q10viking

---
+++ basic/Initals.java
@@ -0,0 +1,15 @@
+public class Initals{
+    public static void main(String[] args){
+    int i=0;
+    for(i=0;i/5==0;i++) System.out.println();
+    System.out.println("     ***      *     ***      *     *    *****    *   *    *****    *   *    ***    ");
+    System.out.println("    *   *     *    *   *     *     *      *      *  *       *      *   *   *   *   ");
+    System.out.println("    *   *     *    *   *     *     *      *      * *        *      **  *   *       ");
+    System.out.println("    *   *     *    *   *     *     *      *      **         *      * * *   * ***   ");
+    System.out.println("    * * *     *    *   *     *     *      *      * *        *      *  **   *   *   ");
+    System.out.println("    *  *      *    *   *      *   *       *      *  *       *      *   *   *   *   ");
+    System.out.println("     ** * *   *     ***         *       *****    *   *    *****    *   *    ***    ");
+    for(i=0;i/5==0;i++) System.out.println();
+    }
+
+}
